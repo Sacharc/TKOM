e237757f1b262f3beeddf75c008f17cceedde892
Sacharc <mi3uss@gmail.com>
2016-05-10 22:52:44 +0200

    include string w TokenType

--- TokenType.h
+++ TokenType.h
@@ -5,8 +5,10 @@
 #ifndef PROJECT_TOKENTYPE_H
 #define PROJECT_TOKENTYPE_H
 
+#include <string>
+
 enum TokenType {
-    Name,
+    Name = 1,
     Number,
     Separator,
     OpenIndexBracket,
@@ -37,5 +39,41 @@ enum TokenType {
     Multiply,
     Divide,
 };
+
+inline std::string getTokenName(int token) {
+    switch(token) {
+        case Name:              return "Name";
+        case Number:            return "Number";
+        case Separator:         return "Separator";
+        case OpenIndexBracket:  return "OpenIndexBracket";
+        case CloseIndexBracket: return "CloseIndexBracket";
+        case Sublist:           return "Sublist";
+        case OpenListBracket:   return "OpenListBracket";
+        case CloseListBracket:  return "CloseListBracket";
+        case OpenBracket:       return "OpenBracket";
+        case CloseBracket:      return "CloseBracket";
+        case If:                return "If";
+        case Else:              return "Else";
+        case Foreach:           return "Foreach";
+        case End:               return "End";
+        case Filter:            return "Filter";
+        case Size:              return "Size";
+        case Sort:              return "Sort";
+        case Assignment:        return "Assignment";
+        case Or:                return "Or";
+        case And:               return "And";
+        case Equality:          return "Equality";
+        case Inequality:        return "Inequality";
+        case Less:              return "Less";
+        case Greater:           return "Greater";
+        case LessOrEqual:       return "LessOrEqual";
+        case GreaterOrEqual:    return "GreaterOrEqual";
+        case Plus:              return "Plus";
+        case Minus:             return "Minus";
+        case Multiply:          return "Multiply";
+        case Divide:            return "Divide";
+        default:                return "";
+    }
+}
 
 #endif //PROJECT_TOKENTYPE_H
--- main.cc
+++ main.cc
@@ -1,10 +1,16 @@
 #include <iostream>
-#include <FlexLexer.h> 
+#include <FlexLexer.h>
+#include "TokenType.h"
 
 int main()
 {
     FlexLexer* lexer = new yyFlexLexer;
-    while(lexer->yylex() != 0);
+    while(1) {
+        int token = lexer->yylex();
+        if(token == 0)
+            break;
+        std::cout<< getTokenName(token) <<" " <<lexer->YYText() <<std::endl;
+    }
 
     return 0;
 }
